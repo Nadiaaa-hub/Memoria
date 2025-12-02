f88f349d3764b34c8ee6dbe9f90ca74c7d4b4e5a
Nadia <nadya2000006@gmail.com>
2025-12-02 19:54:19 +0200

    done project

--- Memoria-project/src/main.js
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init();
---
+++ memoria/main.js
@@ -0,0 +1,9 @@
+import "./style.css";
+import { setupCounter } from "./counter.js";
+
+setupCounter(document.querySelector("#counter"));
+import "./style.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
